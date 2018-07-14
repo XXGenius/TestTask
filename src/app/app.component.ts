61896fc3e348c873b440bdb5222d6356714517da
genius <djedai.genius@gmail.com>
2018-07-15 01:46:55 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { OrderPipe } from 'ngx-order-pipe';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  cards: any = [];
+  memory: any = [];
+  flags = true;
+  addForm: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder, private orderPipe: OrderPipe) {
+    this.memory = localStorage.getItem('cards');
+    this.cards = JSON.parse(this.memory);
+  }
+
+
+  ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      id: [''],
+      num: ['', Validators.required],
+      invoice: ['', Validators.required],
+      client: ['', Validators.required],
+      time: ['', Validators.required]
+    });
+
+  }
+
+  delete(index): void {
+    this.cards = this.cards.filter( card => card !== this.cards[index]);
+    localStorage.setItem( 'cards', JSON.stringify(this.cards));
+  }
+
+  flag(): void {
+    console.log(this.flags);
+    this.flags = !this.flags;
+  }
+
+  updateData(): void {
+    this.memory = localStorage.getItem('cards');
+    this.cards = JSON.parse(this.memory);
+  }
+
+  onSubmit(): void {
+    this.cards = this.cards || [];
+    console.log(this.addForm.value);
+    this.cards.push(this.addForm.value);
+    localStorage.setItem( 'cards', JSON.stringify(this.cards));
+   }
+
+   order(arg): void {
+       this.cards = this.orderPipe.transform(this.cards, arg);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import {NgModule, Pipe} from '@angular/core';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {CalendarModule} from 'primeng/primeng';
+import {
+  MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule,
+  MatRippleModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {OrderPipe} from 'ngx-order-pipe';
+import {DndModule} from 'ng2-dnd';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    CalendarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    DndModule.forRoot()
   ],
-  providers: [],
+  providers: [FormBuilder, OrderPipe ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
